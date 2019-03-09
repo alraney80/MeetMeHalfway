30237d644690f1e6c816ee9eb2ccdfd300643909
Miranda Bigby <mirandabigby@my.unt.edu>
2019-03-09 16:29:01 -0600

    Added functionality to the button on the landing page.

--- app/src/main/java/com/example/meetmehalfway/LandingPage.java
+++ app/src/main/java/com/example/meetmehalfway/LandingPage.java
@@ -1,13 +1,17 @@
 package com.example.meetmehalfway;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class LandingPage extends AppCompatActivity {
+
+    private Button enterButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +19,18 @@ public class LandingPage extends AppCompatActivity {
         setContentView(R.layout.activity_landing_page);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        enterButton = findViewById(R.id.enterButton);
+        enterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OpenMapsActivity();
+            }
+        });
     }
 
+    public void OpenMapsActivity() {
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
+    }
 }
