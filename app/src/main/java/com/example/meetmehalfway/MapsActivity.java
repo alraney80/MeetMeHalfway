ffdb8ce7d6f8072b63a2b9197ad910d637d5f800
Miranda Bigby <mirandabigby@my.unt.edu>
2019-03-24 18:10:22 -0500

    Added code to make the map zoom in to the points.

--- app/src/main/java/com/example/meetmehalfway/MapsActivity.java
+++ app/src/main/java/com/example/meetmehalfway/MapsActivity.java
@@ -4,11 +4,14 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
@@ -41,5 +44,21 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.addMarker(new MarkerOptions().position(addr1).title("Address 1"));
         mMap.addMarker(new MarkerOptions().position(addr2).title("Address 2"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(addr1));
+
+
+
+            // This zooms in the map so that you only see the two addresses instead of the world view.
+        LatLngBounds.Builder builder = new LatLngBounds.Builder();
+        //the include method will calculate the min and max bound.
+        builder.include(new MarkerOptions().position(addr1).getPosition());
+        builder.include(new MarkerOptions().position(addr2).getPosition());
+        LatLngBounds bounds = builder.build();
+        int width = getResources().getDisplayMetrics().widthPixels;
+        int height = getResources().getDisplayMetrics().heightPixels;
+        int padding = (int) (width * 0.10); // offset from edges of the map 10% of screen
+        CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(bounds, width, height, padding);
+        mMap.animateCamera(cu);
+        //End Zoom Code
+
     }
 }
