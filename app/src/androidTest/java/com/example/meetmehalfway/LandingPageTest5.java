dd161c4ef828e142aa16aeae5374ffd4610e3343
Miranda Bigby <mirandabigby@my.unt.edu>
2019-04-25 13:03:41 -0500

    radius error checking, zooming to circle, filtering out "cities".

--- app/src/androidTest/java/com/example/meetmehalfway/LandingPageTest5.java
+++ app/src/androidTest/java/com/example/meetmehalfway/LandingPageTest5.java
@@ -20,50 +20,27 @@ import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
-public class LandingPageTest {
+public class LandingPageTest5 {
 
     @Rule
     public ActivityTestRule<LandingPage> mActivityTestRule = new ActivityTestRule<>(LandingPage.class);
 
     @Test
-    public void landingPageTest() {
-        ViewInteraction editText = onView(
-                allOf(withId(R.id.places_autocomplete_search_input), withText("Search"),
+    public void landingPageTest5() {
+        ViewInteraction imageView = onView(
+                allOf(withId(R.id.main_app_title),
                         childAtPosition(
-                                allOf(withId(R.id.autocomplete_frag_address1),
+                                allOf(withId(R.id.coordinatorLayout),
                                         childAtPosition(
-                                                withId(R.id.coordinatorLayout),
-                                                2)),
-                                1),
+                                                withId(android.R.id.content),
+                                                0)),
+                                0),
                         isDisplayed()));
-        editText.check(matches(withText("8176 county road 2419 royse city tx 75189")));
-
-        ViewInteraction editText2 = onView(
-                allOf(withId(R.id.places_autocomplete_search_input), withText("Search"),
-                        childAtPosition(
-                                allOf(withId(R.id.autocomplete_frag_address1),
-                                        childAtPosition(
-                                                withId(R.id.coordinatorLayout),
-                                                2)),
-                                1),
-                        isDisplayed()));
-        editText2.check(matches(withText("8176 county road 2419 royse city tx 75189")));
-
-        ViewInteraction editText3 = onView(
-                allOf(withId(R.id.places_autocomplete_search_input), withText("Search"),
-                        childAtPosition(
-                                allOf(withId(R.id.autocomplete_frag_address2),
-                                        childAtPosition(
-                                                withId(R.id.coordinatorLayout),
-                                                3)),
-                                1),
-                        isDisplayed()));
-        editText3.check(matches(withText("455 Kinney dr keller tx ")));
+        imageView.check(matches(isDisplayed()));
 
         ViewInteraction button = onView(
                 allOf(withId(R.id.enterButton),
@@ -75,17 +52,6 @@ public class LandingPageTest {
                                 5),
                         isDisplayed()));
         button.check(matches(isDisplayed()));
-
-        ViewInteraction button2 = onView(
-                allOf(withId(R.id.enterButton),
-                        childAtPosition(
-                                allOf(withId(R.id.coordinatorLayout),
-                                        childAtPosition(
-                                                withId(android.R.id.content),
-                                                0)),
-                                5),
-                        isDisplayed()));
-        button2.check(matches(isDisplayed()));
     }
 
     private static Matcher<View> childAtPosition(
